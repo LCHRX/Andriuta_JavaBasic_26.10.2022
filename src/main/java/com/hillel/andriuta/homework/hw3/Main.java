d7682eb0ff3fc0987a218c48411d4835d084d5d8
Коstiantyn Аndriuta <spacekotek@gmail.com>
2022-11-15 16:19:18 +0200

    homework3

--- src/main/java/com/hillel/andriuta/homework/hw3/Main.java
+++ src/main/java/com/hillel/andriuta/homework/hw3/Main.java
@@ -1,4 +1,9 @@
 package com.hillel.andriuta.homework.hw3;
 
 public class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 24;
+        System.out.println(a+b);
+    }
 }
