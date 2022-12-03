df247245f2648047ed39bd6d787ca7fb0c4a6e89
Andriuta Constantine <spacekotek@gmail.com>
2022-12-03 23:10:22 +0200

    just testing

--- src/main/java/com/hillel/andriuta/homework/hw3/Main.java
+++ src/main/java/com/hillel/andriuta/homework/hw3/Main.java
@@ -3,7 +3,7 @@ package com.hillel.andriuta.homework.hw3;
 public class Main {
     public static void main(String[] args) {
         int a = 6;
-        int b = 4;
+        int b = 6;
         System.out.println(a-b);
     }
 }
