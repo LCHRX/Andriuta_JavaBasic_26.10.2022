5f2659e199cbd789ca9ae2b89a1f80bb5d057fd1
Andriuta Constantine <spacekotek@gmail.com>
2022-12-04 01:36:47 +0200

    Homework4

---
+++ src/main/java/com/hillel/andriuta/homework/hw4/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.andriuta.homework.hw4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int h = 12;
+        int P = (a+b+h)*4;
+        int V = a*b*h;
+        System.out.println("Периметр = " + P);
+        System.out.println("Об'єм = " + V);
+    }
+}
