aecd95c42b5fce86cbca3082d0a1c7c041fb1b32
Andriuta Constantine <spacekotek@gmail.com>
2022-12-08 01:57:42 +0200

    Homework7

--- src/main/java/com/hillel/andriuta/homework/hw7/Main.java
+++ src/main/java/com/hillel/andriuta/homework/hw7/Main.java
@@ -1,21 +1,44 @@
 package com.hillel.andriuta.homework.hw7;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int a = (int) (Math.random() * 11);
         Scanner scanner = new Scanner(System.in);
-        for (int i = 10; i > 0; i--) {
-            System.out.println("Please enter the number(You have " + i + " attempts left)");
-            int b = scanner.nextInt();
-            if (b == a) {
-                System.out.println("You guessed the number! Congrats");
-                break;
-            } else {
-                System.out.println("Wrong number");
+        int attempts = 10;
+        int i = attempts;
+        System.out.println("Please enter the number(You have " + i + " attempts left)");
+        while (i > 0) {
+           if (scanner.hasNextInt()) {
+               int b = scanner.nextInt();
+               if (b == a) {
+                   System.out.println("You guessed the number! Congrats");
+                   break;
+               } else if (b > 10 || b < 0) {
+                   System.out.println("Enter a number from 1 to 10!");
+                   continue;
+               } else if (b < a && i != 1) {
+                   System.out.println("The number is greater than your guess. You have " + (i - 1) + " attempts left.");
+               } else if (b > a && i != 1) {
+                   System.out.println("The number is lesser than your guess. You have " + (i - 1) + " attempts left.");
+               }
+               if (i == 1 && b != a) {
+                   System.out.println("You lost!");
+                   break;
+               }
+               i--;
+           }
+           else {
+                System.out.println("Wrong data, try again");
+                scanner.next();
             }
         }
-        System.out.println(a);
+        System.out.println("Your number was: " + a);
     }
 }
+
+
+
+
