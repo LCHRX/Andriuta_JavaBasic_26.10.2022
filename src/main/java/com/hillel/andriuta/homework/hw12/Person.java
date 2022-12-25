c474565af9f5eba7e5c5a4cd699ded25c8645a06
Andriuta Constantine <spacekotek@gmail.com>
2022-12-26 00:42:10 +0200

    Homework 12

---
+++ src/main/java/com/hillel/andriuta/homework/hw12/Person.java
@@ -0,0 +1,14 @@
+package com.hillel.andriuta.homework.hw12;
+
+public class Person {
+
+            public static void main(String[] args) {
+                personInfo("Will", "Smith","New York", "2936729462846");
+                personInfo("Jackie", "Chan", "Shanghai", "12312412412");
+                personInfo("Sherlock","Holmes","London","37742123513");
+            }
+            static void personInfo(String name, String surname, String city, String number){
+                System.out.println("Зателефонувати громадянину " + name + " " + surname + " із міста " + city +" можна за номером " + number);
+            }
+        }
+
