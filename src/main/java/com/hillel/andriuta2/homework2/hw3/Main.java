9b37cceabedc40ebcdd5078318c2f44a7309c320
Andriuta Constantin <coandriuta@gmail.com>
2024-08-07 16:53:02 +0300

    commit test

---
+++ src/main/java/com/hillel/andriuta2/homework2/hw3/Main.java
@@ -0,0 +1,11 @@
+package com.hillel.andriuta2.homework2.hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+    }
+}
