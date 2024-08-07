1957ca5b741c711de01121c29ca92722c6caa256
Andriuta Constantin <coandriuta@gmail.com>
2024-08-07 16:37:53 +0300

    git init

---
+++ src/main/java/com/hillel/andriuta2/homework2/hw2/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.andriuta2.homework2.hw2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double lat = 39.916668;
+        double longit = 116.383331;
+
+        System.out.println("Beijing lattitude is " + lat +  "\u00B0, longtitude is " + longit + "\u00B0");
+
+
+    }
+}
